4ed6bc748edadfd26a4c9636bf9f051c28db045a
Connor Imes <connor.k.imes@gmail.com>
2015-07-20 15:45:49 -0500

    Remove native logging support in wrapper and handle in rust

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,75 +1,81 @@
 use pow::*;
 use std::mem;
-#[cfg(unix)]
+use std::io::{self, Write};
 use std::fs::File;
-#[cfg(unix)]
-use std::os::unix::io::AsRawFd;
 
 /// Contains the Heartbeat and its window data buffer.
 pub struct HeartbeatPow {
     pub hb: heartbeat_pow_context,
     pub hbr: Vec<heartbeat_pow_record>,
+    pub log: Option<File>,
 }
 
 impl HeartbeatPow {
     /// Allocate and initialize a new `Heartbeat`.
     pub fn new(window_size: usize,
-               hwc_callback: Option<heartbeat_pow_window_complete>) -> Result<HeartbeatPow, &'static str> {
-        let mut hbr = Vec::with_capacity(window_size);
+               hwc_callback: Option<heartbeat_pow_window_complete>,
+               mut log: Option<File>) -> Result<HeartbeatPow, &'static str> {
+        let mut hbr: Vec<heartbeat_pow_record> = Vec::with_capacity(window_size);
         unsafe {
+            // must explicitly set size so we can read data later
+            // (Rust isn't aware of native code modifying the buffer)
+            hbr.set_len(window_size);
             let mut hb = mem::uninitialized();
-            match heartbeat_pow_init(&mut hb, hbr.capacity() as u64, hbr.as_mut_ptr(), hwc_callback) {
-                0 => Ok(HeartbeatPow { hb: hb, hbr: hbr, }),
+            match heartbeat_pow_init(&mut hb,
+                                     hbr.capacity() as u64,
+                                     hbr.as_mut_ptr(),
+                                     hwc_callback) {
+                0 => {
+                    if let Some(ref mut l) = log {
+                        l.write_all("HB    Tag    Work    Start_Time    End_time    \
+                            Global_Perf    Window_Perf    Instant_Perf    \
+                            Start_Energy    End_Energy    \
+                            Global_Pwr    Window_Pwr    Instant_Pwr\n".as_bytes()).unwrap()
+                    }
+                    Ok(HeartbeatPow { hb: hb, hbr: hbr, log: log, })
+                },
                 _ => Err("Failed to initialize heartbeat")
             }
         }
     }
 
     /// Issue a heartbeat
-    pub fn heartbeat_pow(&mut self,
-                         tag: u64,
-                         work: u64,
-                         start_time: u64,
-                         end_time: u64,
-                         start_energy: u64,
-                         end_energy: u64) {
+    pub fn heartbeat(&mut self,
+                     tag: u64,
+                     work: u64,
+                     start_time: u64,
+                     end_time: u64,
+                     start_energy: u64,
+                     end_energy: u64) {
         unsafe {
             heartbeat_pow(&mut self.hb, tag, work, start_time, end_time, start_energy, end_energy)
         }
     }
 
-    #[cfg(unix)]
-    pub fn log_window_buffer(&self, log: File, print_header: bool) -> Result<(), &'static str> {
-        let ph: i32 = match print_header {
-            true => 1,
-            false => 0,
-        };
-        unsafe {
-            match heartbeat_pow_log_window_buffer(&self.hb, log.as_raw_fd(), ph) {
-                0 => Ok(()),
-                _ => Err("Error logging window buffer"),
+    fn write_log(r: &heartbeat_pow_record, l: &mut File) -> io::Result<usize> {
+        l.write(format!("{}    {}    {}    {}    {}    \
+                         {}    {}    {}    \
+                         {}    {}    \
+                         {}    {}    {}\n",
+                        r.id, r.user_tag, r.work, r.start_time, r.end_time,
+                        r.perf.global, r.perf.window, r.perf.instant,
+                        r.start_energy, r.end_energy,
+                        r.pwr.global, r.pwr.window, r.pwr.instant).as_bytes())
+    }
+
+    /// Rust-only function that logs the buffer (up to buffer_index) to a file.
+    pub fn log_to_buffer_index(&mut self) -> io::Result<()> {
+        match self.log {
+            Some(ref mut l) => {
+                for i in 0..self.hb.ws.buffer_index {
+                    match HeartbeatPow::write_log(self.hbr.get(i as usize).unwrap(), l) {
+                        Ok(_) => (),
+                        Err(e) => return Err(e),
+                    }
+                }
+                l.flush()
             }
-        }
-    }
-
-    /// Utility function to get the most recent user-specified tag
-    pub fn get_tag(&self) -> u64 {
-        unsafe {
-            hb_pow_get_user_tag(&self.hb)
-        }
-    }
-
-    /// Utility function to get the current window performance.
-    pub fn get_window_perf(&self) -> f64 {
-        unsafe {
-            hb_pow_get_window_perf(&self.hb)
-        }
-    }
-
-    /// Utility function to get the current window power.
-    pub fn get_window_pwr(&self) -> f64 {
-        unsafe {
-            hb_pow_get_window_power(&self.hb)
+            None => Ok(())
         }
     }
 }
@@ -77,27 +83,30 @@ impl HeartbeatPow {
 #[cfg(test)]
 mod test {
     use super::HeartbeatPow;
+    use std::fs::File;
 
     #[test]
     fn test_simple() {
         const TIME_INC: u64 = 1000000000;
         const ENERGY_INC: u64 = 1000000;
-        let mut hb = HeartbeatPow::new(5, None).unwrap();
+        let mut hb = HeartbeatPow::new(5, None, None).unwrap();
         let mut start_time: u64 = 0;
         let mut end_time: u64 = TIME_INC;
         let mut start_energy: u64 = 0;
         let mut end_energy: u64 = ENERGY_INC;
-        let mut tag: u64 = 0;
-        for _ in 0..10 {
-            hb.heartbeat_pow(tag, 1, start_time, end_time, start_energy, end_energy);
-            assert!(hb.get_tag() == tag);
-            assert!(hb.get_window_perf() == 1.0);
-            assert!(hb.get_window_pwr() == 1.0);
-            tag += 1;
+        for tag in 0..10 {
+            hb.heartbeat(tag, 1, start_time, end_time, start_energy, end_energy);
             start_time = end_time;
             end_time += TIME_INC;
             start_energy = end_energy;
             end_energy += ENERGY_INC;
         }
+    }
+
+    #[test]
+    fn test_file() {
+        let mut hb = HeartbeatPow::new(5, None, Some(File::create("foo.log").unwrap())).unwrap();
+        hb.heartbeat(0, 1, 0, 1, 0, 0);
+        hb.log_to_buffer_index().unwrap();
     }
 }
