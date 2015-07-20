0fdb202eb2e7ae55e659eb8a3df20244a4f8b515
Connor Imes <connor.k.imes@gmail.com>
2015-07-20 09:49:25 -0500

    Link against all libs

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
+	println!("cargo:rustc-link-lib=hbs");
+	println!("cargo:rustc-link-lib=hbs-acc");
     println!("cargo:rustc-link-lib=hbs-pow");
+    println!("cargo:rustc-link-lib=hbs-acc-pow");
 }
