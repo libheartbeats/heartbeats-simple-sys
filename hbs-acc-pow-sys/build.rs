61ada5c5d4eb273480b811875bfebb2f559548f7
Connor Imes <connor.k.imes@gmail.com>
2016-01-25 18:18:59 -0600

    Build static libs if libs not found by pkg-config

--- hbs-acc-pow-sys/build.rs
+++ hbs-acc-pow-sys/build.rs
@@ -6,12 +6,15 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    match pkg_config::find_library("hbs-acc-pow-static") {
+    let lib = "hbs-acc-pow";
+    env::set_var("HBS_ACC_POW_STATIC", "");
+    match pkg_config::find_library(&lib) {
         Ok(_) => (),
         Err(_) => {
             let src = PathBuf::from(&env::var_os("CARGO_MANIFEST_DIR").unwrap())
                                    .parent().unwrap().join("heartbeats-simple");
             let build = PathBuf::from(&env::var_os("OUT_DIR").unwrap()).join("_build");
+            let install = build.join("_local_install");
             let target: String = env::var("TARGET").unwrap();
             let target_parts: Vec<&str> = target.split('-').collect();
             let cmake_var = match target_parts[target_parts.len() - 1].starts_with("android") {
@@ -31,11 +34,31 @@ fn main() {
             };
             fs::remove_dir_all(&build).ok();
             fs::create_dir_all(&build).unwrap();
-            run(Command::new("cmake").arg(cmake_var).arg(cmake_gen).arg(src.to_str().unwrap()).current_dir(&build));
-            run(Command::new("make").arg("hbs-acc-pow-static").current_dir(&build));
-            println!("cargo:rustc-link-lib=static=hbs-acc-pow-static");
-            println!("cargo:rustc-link-search=native={}", build.join("lib").display());
+            let prefix = format!("-DCMAKE_INSTALL_PREFIX={}", install.to_str().unwrap());
+            run(Command::new("cmake").arg(prefix).arg("-DBUILD_SHARED_LIBS=false").arg(cmake_var).arg(&cmake_gen)
+                .arg(src.to_str().unwrap()).current_dir(&build));
+            // build and install all libraries locally
+            run(Command::new("make").current_dir(&build));
+            run(Command::new("make").arg("install").current_dir(&build));
+            // run pkg-config on compiled dir to get any transitive dependencies of static lib
+            set_pkg_config_path(&build, !cmake_gen.is_empty());
+            pkg_config::find_library(&lib).unwrap();
         },
+    }
+}
+
+fn set_pkg_config_path(build: &PathBuf, is_windows: bool) {
+    let delimiter = match is_windows {
+        true => ";",
+        false => ":",
+    };
+    let pkgconfig = build.join("pkgconfig");
+    match env::var_os("PKG_CONFIG_PATH") {
+        Some(p) => {
+            let path = format!("{:?}{}{:?}", pkgconfig, delimiter, p);
+            env::set_var("PKG_CONFIG_PATH", &path);
+        },
+        None => env::set_var("PKG_CONFIG_PATH", &pkgconfig),
     }
 }
 
