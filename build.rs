4a31d7eec444a9b1181ab19604c784caa47489e8
Connor Imes <connor.k.imes@gmail.com>
2016-10-24 10:22:06 -0500

    Remove outdated comments

--- build.rs
+++ build.rs
@@ -10,11 +10,7 @@ fn main() {
         Err(_) => {
             let src = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/heartbeats-simple"));
             let mut config = Config::new(&src);
-
-            // build/install everything - the build is fast and portable enough
-            // cmake crate makes it too troublesome to do individual targets since it expects to install
             let dst: PathBuf = config.build();
-            // none of the libraries have transitive dependencies
             println!("cargo:rustc-link-lib=static=heartbeats-simple");
             println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
         },
